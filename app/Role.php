219eada2732d5f36925075553dcca2b9043b33c5
sisay651 <54186497+sisay651@users.noreply.github.com>
2020-03-09 21:35:52 -0700

    little changes

--- app/Role.php
+++ app/Role.php
@@ -10,7 +10,7 @@ class Role extends Model
  
     protected $softDelete = true;
 protected $fillable = ['name'];
-
+protected $table = "roles";
      public function users() {
      
         return $this->belongsToMany(User::class,'roles_users');
