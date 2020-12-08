9222730162f0bed058c6102ac702bdbee56117c4
tadele08 <61330987+tadele08@users.noreply.github.com>
2020-12-08 03:58:34 +0300

    changes

--- resources/views/universityAdmin/edit.blade.php
+++ resources/views/universityAdmin/edit.blade.php
@@ -44,31 +44,7 @@
                         
                 
                         <!--    ---- student id ---- -->
-                        {{-- <div class="form-group row">
-                            <lable for = "student_id" class = "col-sm-1 col-form-label"><h5>offer capacity:</h5></lable>
-                            <div class="col-sm-6">
-                        <input type="number" name="offer_capacity" class="form-control" id="student_id" 
-                                            placeholder="offer capacity" value="{{$company->offer_capacity}}" required>
-                            </div>
-                        </div> --}}
-                <!-- 
-          <div class="form-group row">
-              <lable for = "grade" class = "col-sm-1 col-form-label"><h5>minimum grade</h5></lable>
-              <div class="col-sm-6">
-                  <input type="number" name="grade" class="form-control" id="student_id" placeholder="grade" required value="{{$company->grade}}">
-              </div>
-          </div>
-    
-              
-          <div class="form-group row">
-            <lable for = "skills" class = "col-sm-1 col-form-label"><h5>other required skils</h5></lable>
-            <div class="col-sm-6">
-                <input type="text" name="skills" class="form-control" id="student_id" placeholder="other skills" required>
-            </div>
-        </div>
-          
-           -->
-<!--    ----register ---- -->
+                       
           <div class="form-group row">
             <div class="col-sm-6 pull-right">
               <button class="btn btn-success" type="submit"> update</button>
